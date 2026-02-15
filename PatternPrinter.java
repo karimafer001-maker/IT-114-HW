12c1db925496f3f3448266622febc72c7a00cb84
Someone <karim.afer001@umb.edu>
2026-02-15 00:21:33 -0500

    Add pattern printing homework for IT-114-HW

---
+++ PatternPrinter.java
@@ -0,0 +1,10 @@
+public class PatternPrinter { 
+    public static void main(String[] args) { 
+        System.out.println("   +IIIIIIIII+");
+        System.out.println(" [ |  o   o  | ] ");
+        System.out.println("   |    ^    | ");
+        System.out.println("   |   '-'   | ");
+        System.out.println("   +---------+ ");
+    }
+}
+
